c850393d2299af2ee900a1aad79c5a1879f3d1a6
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-05-01 18:55:18 -0700

    added the character select script and made the buttons functional

---
+++ Assets/Scripts/Helper/CharacterSelect.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public GameObject[] available_Heroes;
+    public Text selectedText;
+    public GameObject starIcon;
+    public Image selectbtn;
+    public Sprite button_green, button_blue;
+    public Text starScoreText;
+
+    private int currentIndex;
+    private bool[] heroes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void InitializeCharacters()
+    {
+        currentIndex = 0;
+        for(int i =0; i < available_Heroes.Length; i++)
+        {
+            available_Heroes[i].SetActive(false);
+        }
+        available_Heroes[currentIndex].SetActive(true);
+        //heroes = GameManager;
+    }
+
+    public void NextHero()
+    {
+        available_Heroes[currentIndex].SetActive(false);
+        if(currentIndex + 1 == available_Heroes.Length)
+        {
+            currentIndex = 0;
+        }
+        else
+        {
+            currentIndex++;
+        }
+        available_Heroes[currentIndex].SetActive(true);
+    }
+
+    public void PreviousHero()
+    {
+        available_Heroes[currentIndex].SetActive(false);
+        if (currentIndex - 1 == -1)
+        {
+            currentIndex = available_Heroes.Length-1;
+        }
+        else
+        {
+            currentIndex--;
+        }
+        available_Heroes[currentIndex].SetActive(true);
+    }
+
+
+}
---
+++ Assets/Scripts/Helper/MainMenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject hero_Menu;
+    public Text starScoreText;
+    public Image music_Img;
+    public Sprite music_on, music_off;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HeroMenu()
+    {
+        hero_Menu.SetActive(true);
+        //display star score
+    }
+
+    public void HomeButton()
+    {
+        hero_Menu.SetActive(false);
+    }
+}
