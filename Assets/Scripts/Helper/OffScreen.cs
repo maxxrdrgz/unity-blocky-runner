db9ec23021640a7b94e72784d686d9a02f78de0f
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-05-14 20:42:39 -0700

    added comments

--- Assets/Scripts/Helper/OffScreen.cs
+++ Assets/Scripts/Helper/OffScreen.cs
@@ -9,9 +9,13 @@ public class OffScreen : MonoBehaviour
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        print(gameObject);
     }
 
-    // Update is called once per frame
+    /** 
+        This function will check to see if a tile is out of the main cameras
+        view
+    */
     void Update()
     {
         //this will return 6 planes that form the camera view
@@ -25,6 +29,11 @@ public class OffScreen : MonoBehaviour
         }
     }
 
+    /** 
+        This function will check which gameobject this script is currently acting
+        upon. Afterwards, it will change the position of the tile to it's last
+        known position, plus the offset, using it's last know order in layer.
+    */
     void CheckTile()
     {
         if(this.tag == Tags.ROAD)
@@ -74,6 +83,14 @@ public class OffScreen : MonoBehaviour
         }
     }
 
+    /** 
+        This function will change the transform position, order in layer of the
+        gameobject this script is attached to.
+
+        @param {ref Vector3} reference to last knowm position of gameobject
+        @param {Vector3} coordinates to offset the gameobjects position
+        @param {ref int} order in layer
+    */
     void Change(ref Vector3 pos, Vector3 offSet, ref int orderLayer)
     {
         transform.position = pos;
